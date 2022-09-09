db9c9399e6a7396d99ea75551825517d4ccb479d
Hatim Zahid <hatimzahidhusain@gmail.com>
2022-09-09 15:40:36 +0000

    Finish RedoingModels

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,13 @@
-from django.contrib import admin
 from blog.models import Tag, Post
-
+from django.contrib import admin
 
 admin.site.register(Tag)
+
+class PostAdmin(admin.ModelAdmin):
+  prepopulated_fields = {"slug": ("title",)}
+
 admin.site.register(Post, PostAdmin)
 
 
-class PostAdmin(admin.ModelAdmin):
-  prepopulated_fields = {"slug" : {"title"}}
 
 # Register your models here.
