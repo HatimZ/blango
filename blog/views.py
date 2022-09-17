4c969e01738d315c396e3cd25db14ac761b15f53
Hatim Zahid <hatimzahidhusain@gmail.com>
2022-09-17 08:33:52 +0000

    Finish Custom Filters

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from blog.models import Post
 
 # Create your views here.
+user_model = get_user_model()
+
 
 
 def index(request):
-  return render(request , "blog/index.html")
+    posts = Post.objects.filter(published_at__lte = timezone.now())
 
+    return render(request , "blog/index.html" , {'posts' : posts} )
+
+
+def author_details(author):
+    if not isinstance(author, user_model):
+      
+        return ""
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    return name
